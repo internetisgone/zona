80b808fe39bfdee28289ebe365afecf00b6fdfd0
internetisgone <108741687+internetisgone@users.noreply.github.com>
2024-04-18 00:50:24 +0800

    basic terrain

--- Assets/Scripts/Stalkers/NPC/StalkerNPC.cs
+++ Assets/Scripts/Stalkers/NPC/StalkerNPC.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.UI.GridLayoutGroup;
 
 //[Flags]
 //public enum StalkerState
