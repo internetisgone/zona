4aea8da7b569931fccc1429ffccab3634f56debc
internetisgone <108741687+internetisgone@users.noreply.github.com>
2024-04-25 18:55:08 +0800

    Update copy_textures.cmd

--- Assets/Scripts/Utils/copy_textures.cmd
+++ Assets/Scripts/Utils/copy_textures.cmd
@@ -3,14 +3,13 @@
 : for every arg, eg trees_bark_a_01, this script will 
 : copy the matching dds texture, and bump map if any, to the destination 
 
-: the following paths should not contain spaces
 set source=D:\X-Ray_CoP_SDK\editors\gamedata\textures
 set destination=D:\CoP_textures
 
 pushd %source%
 
 for %%t in (%*) do (
-    for /f %%G in ('dir /b/s %%t*.dds') do copy %%G %destination%
+    for /f %%G in ('dir /b/s %%t*.dds') do (copy %%G %destination% | echo copied %%G)
 )
 
 popd
